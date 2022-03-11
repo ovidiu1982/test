7e69425aefd85e7c5784315f3766b995e7b78cbc
ovidiu1982 <ovidiuserban1982@gmail.com>
2022-03-11 22:27:29 +0000

    Ovi

---
+++ script.js
@@ -0,0 +1,16 @@
+var map = L.map('map', {
+    'center': [0, 0],
+    'zoom': 0,
+    'layers': [
+      L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        'attribution': 'Map data &copy; OpenStreetMap contributors'
+      })
+    ]
+  });
+  
+  var geojsonData = "ev_stations_v1.geojson"
+  
+  var geojsonLayer = L.geoJson(geojsonData).addTo(map);
+  
+  map.fitBounds(geojsonLayer.getBounds());
+  
