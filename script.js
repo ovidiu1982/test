37bac0a7884c2450f5486a73b881a424f1a97a2d
ovidiu1982 <ovidiuserban1982@gmail.com>
2022-03-11 22:55:21 +0000

    Ovi

--- script.js
+++ script.js
@@ -1,41 +1,33 @@
 var map = L.map('map', {
-    'center': [0, 0],
-    'zoom': 0,
-    'layers': [
-      L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        'attribution': 'Map data &copy; OpenStreetMap contributors'
-      })
-    ]
-  });
-  
-  var geojsonData = {
-    "type": "FeatureCollection",
-   "features": [
-  {
-    "type": "Feature",
-    "geometry": {
-       "type": "Point",
-       "coordinates":  [ -118.3879714,34.24831915 ]
+  'center': [0, 0],
+  'zoom': 0,
+  'layers': [
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      'attribution': 'Map data &copy; OpenStreetMap contributors'
+    })
+  ]
+});
+
+var geojsonData = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates":  [ -118.448504,34.052542 ]
+      }
     },
-    "properties": {
-    "Station_Name":"LADWP - Truesdale Center",
-    "City":"Sun Valley",
-    "State":"CA",
-    "ZIP_Code":"91352",
-    "Status_Code":"E",
-    "EV_Network":"Non-Networked",
-    "Owner_Type_Code":"LG",
-    "EV_Level2_EVSE_Num":39,
-    "Open_Date":"15/10/1999",
-    "EV_Connector_Types":"CHADEMO J1772 J1772COMBO",
-    "Facility_Type":"UTILITY",
-    "EV_Pricing":""
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates":  [ -118.271387,34.040539 ]
+      }
     }
-  }
-    ]
-  };
-  
-  var geojsonLayer = L.geoJson(geojsonData).addTo(map);
-  
-  map.fitBounds(geojsonLayer.getBounds());
-  
+  ]
+};
+
+var geojsonLayer = L.geoJson(geojsonData).addTo(map);
+
+map.fitBounds(geojsonLayer.getBounds());
